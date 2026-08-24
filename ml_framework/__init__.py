1045d9946547573568bf4b40c719afbbe233ad2e
itchinda <100134993+itchinda@users.noreply.github.com>
2026-08-24 09:52:34 -0400

    Clean __init__.py file.

--- ml_framework/__init__.py
+++ ml_framework/__init__.py
@@ -1,6 +1,5 @@
 # ml_framework/__init__.py
 
-# Importe les classes depuis tes sous-modules pour les rendre directement accessibles
 from .layers import LinearLayer, ActivationLayer, Conv2D, MaxPool2D, Flatten, Dropout
 from .activations import ReLU, ReLUDerivative, Sigmoid, SigmoidDerivative
 from .losses import BinaryCrossEntropyLoss, CrossEntropyLoss
